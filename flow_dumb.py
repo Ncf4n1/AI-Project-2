66405464c6ba296f3bf387cd089ae69acd95fb97
Logan Vining <llvinin@hotmail.com>
2018-11-05 13:42:08 -0700

    Colors find first solution unless none available. Need to backgrack color solutions to find solution that fills board

--- flow_dumb.py
+++ flow_dumb.py
@@ -1,3 +1,5 @@
+import time
+
 def init_maze(maze):
 
     # Read in the given file line by line until the end of file
@@ -20,12 +22,12 @@ def init_vars(maze):
     csp_vars = []
     for line in maze:
         for item in line:
-            if item != '_' and item not in csp_vars:
+            if item != '_' and item != '\n' and item not in csp_vars:
                 csp_vars.append(item)
 
     return csp_vars
 
-def find_start(maze):
+def find_var_start(maze, var):
     start_x = 0
     start_y = 0
 
@@ -33,7 +35,7 @@ def find_start(maze):
         for item in line:
             found_item = False
 
-            if item == '_':
+            if item == '_' or item != var:
                 start_x += 1
             else:
                 found_item = True
@@ -47,6 +49,28 @@ def find_start(maze):
 
     return None
 
+def find_var_final(maze, var):
+    final_x = 0
+    final_y = 0
+    count = 0
+
+    for line in maze:
+        for item in line:
+            found_item = False
+
+            if item == '_' or item != var:
+                final_x += 1
+            else:
+                found_item = True
+                count += 1
+                break
+
+        if not found_item or count < 2:
+            final_y += 1
+            final_x = 0
+        else:
+            return [final_x, final_y]
+
 def check_finished(maze):
     for line in maze:
         for item in line:
@@ -55,40 +79,100 @@ def check_finished(maze):
 
     return True
 
-def backtrack(maze, current_var, current_x, current_y):
+def zig_zag(maze, current_var, current_x, current_y):
+    if (current_x - 1 >= 0 and maze[current_y][current_x - 1] == current_var):
+        if (current_y - 1 >= 0 and maze[current_x - 1][current_y - 1] == current_var):
+            if (current_y - 1 >= 0 and maze[current_y - 1][current_x] == current_var):
+                return True
+
+    if (current_y - 1 >= 0 and maze[current_y - 1][current_x] == current_var):
+        if (current_x + 1 < len(maze) and maze[current_y - 1][current_x + 1] == current_var):
+            if (maze[current_y][current_x + 1] == current_var):
+                return True
+
+    if (current_x + 1 < len(maze) and maze[current_y][current_x + 1] == current_var):
+        if (current_y + 1 < len(maze) and maze[current_y + 1][current_x + 1] == current_var):
+            if (maze[current_y + 1][current_x] == current_var):
+                return True
+
+    if (current_y + 1 < len(maze) and maze[current_y + 1][current_x] == current_var):
+        if (current_x - 1 >= 0 and maze[current_y + 1][current_x - 1] == current_var):
+            if (maze[current_y][current_x - 1] == current_var):
+                return True
+
+    return False
+
+def backtrack(maze, current_var, start_x, start_y, current_x, current_y, final_x, final_y):
+    found = False
     if check_finished(maze):
         return
     else:
         print_maze(maze)
-        if ( current_x - 1 >= 0 and maze[current_y][current_x - 1] == '_'):
-            print('called 1')
-            maze[current_y][current_x - 1] = current_var
-            current_x = current_x - 1
-            return backtrack(maze, current_var, current_x, current_y)
+        if (current_x - 1 >= 0 and (current_x - 1 == final_x and current_y == final_y)):
+            return True
+        elif (current_x - 1 >= 0 and maze[current_y][current_x - 1] == '_'):
+            print('called left')
+            if not zig_zag(maze, current_var, current_x - 1, current_y):
+                maze[current_y][current_x - 1] = current_var
+                found = backtrack(maze, current_var, start_x, start_y, current_x - 1, current_y, final_x, final_y)
 
-        elif ( current_y - 1 >= 0 and maze[current_y - 1][current_x] == '_'):
-            print('called 2')
-            maze[current_y - 1][current_x] = current_var
-            current_y = current_y - 1
-            return backtrack(maze, current_var, current_x, current_y)
+        if (current_y - 1 >= 0 and (current_x == final_x and current_y - 1 == final_y)):
+            return True
+        elif (current_y - 1 >= 0 and maze[current_y - 1][current_x] == '_'):
+            print('called top')
+            if not zig_zag(maze, current_var, current_x, current_y - 1):
+                maze[current_y - 1][current_x] = current_var
+                found = backtrack(maze, current_var, start_x, start_y, current_x, current_y - 1, final_x, final_y)
 
-        elif ( current_x + 1 <= len(maze) and maze[current_y][current_x + 1] == '_'):
-            print('called 3')
-            maze[current_y][current_x + 1] = current_var
-            current_x = current_x + 1
-            return backtrack(maze, current_var, current_x, current_y)
+        if (current_x + 1 < len(maze) and (current_x + 1 == final_x and current_y == final_y)):
+            return True
+        elif (current_x + 1 < len(maze) and maze[current_y][current_x + 1] == '_'):
+            print('called right')
+            if not zig_zag(maze, current_var, current_x + 1, current_y):
+                maze[current_y][current_x + 1] = current_var
+                found = backtrack(maze, current_var, start_x, start_y, current_x + 1, current_y, final_x, final_y)
 
-        elif ( current_y + 1 <= len(maze) and maze[current_y + 1][current_x] == '_'):
-            print('called 4')
-            maze[current_y + 1][current_x] = current_var
-            current_y = current_y + 1
-            return backtrack(maze, current_var, current_x, current_y)
+        if (current_y + 1 < len(maze) and (current_x == final_x and current_y + 1 == final_y)):
+            return True
+        elif (current_y + 1 < len(maze) and maze[current_y + 1][current_x] == '_'):
+            print('called down')
+            if not zig_zag(maze, current_var, current_x, current_y + 1):
+                maze[current_y + 1][current_x] = current_var
+                found = backtrack(maze, current_var, start_x, start_y, current_x, current_y + 1, final_x, final_y)
+
+        if not found:
+            print('start x = ' + str(start_x) + ' & start y = ' + str(start_y))
+            print('current x = ' + str(current_x) + ' & current y = ' + str(current_y))
+            if not (current_x == start_x and current_y == start_y):
+                print('entered')
+                maze[current_y][current_x] = '_'
+            return False
+
+        return True
 
 def main():
     maze = []
     init_maze(maze)
     vars = init_vars(maze)
-    start = find_start(maze)
-    backtrack(maze, vars[0], start[0], start[1])
+    start = find_var_start(maze, vars[0])
+    current_x = start[0]
+    current_y = start[1]
+    final = find_var_final(maze, vars[0])
+    final_x = final[0]
+    final_y = final[1]
+
+    while len(vars) > 0:
+        backtrack(maze, vars[0], current_x, current_y, current_x, current_y, final_x, final_y)
+        print('****Finished Color')
+        vars.remove(vars[0])
+        if len(vars) > 0:
+            current_coords = find_var_start(maze, vars[0])
+            final_coords = find_var_final(maze, vars[0])
+            current_x = current_coords[0]
+            current_y = current_coords[1]
+            final_x = final_coords[0]
+            final_y = final_coords[1]
+        else:
+            break
 
 main()
